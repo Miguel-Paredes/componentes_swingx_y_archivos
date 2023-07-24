2998cdd5385e9a3cdfc1a29273053cce185281c0
AzureAD\10800 <os.miguelparedes@gmail.com>
2023-07-23 19:53:53 -0500

    update

--- src/usuario.java
+++ src/usuario.java
@@ -1,3 +1,28 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class usuario {
-    private javax.swing.JPanel JPanel;
+    public JPanel rootPanel;
+    private JButton salirButton;
+    public JTextField datostxt;
+
+    public usuario(){
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame1;
+                frame1 = (JFrame) SwingUtilities.getWindowAncestor(rootPanel);
+                frame1.setVisible(false);
+
+                JFrame frame2 = new JFrame("Login");
+                inicio ventanaPrincipal = new inicio ();
+                frame2.setContentPane(ventanaPrincipal.JPanel);
+                frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame2.pack();
+                frame2.setVisible(true);
+
+            }
+        });
+    }
 }
