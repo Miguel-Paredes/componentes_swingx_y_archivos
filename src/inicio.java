85279907cc94aa7911fff13fcfa55dfc99452d0c
AzureAD\10800 <os.miguelparedes@gmail.com>
2023-07-23 19:08:17 -0500

    update

--- src/inicio.java
+++ src/inicio.java
@@ -7,4 +7,16 @@ public class inicio {
     private JLabel mensaje2;
     private JLabel usuario;
     private JLabel contrasenia;
+    private JButton inicio;
+    private JButton registrarse;
+    private JTextField textField1;
+    private JTextField textField2;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("inicio");
+        frame.setContentPane(new inicio().Jpanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
