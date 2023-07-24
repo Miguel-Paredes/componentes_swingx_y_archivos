37fbf983d5d2d13e9f169f80ceed7a12e90530fa
AzureAD\10800 <os.miguelparedes@gmail.com>
2023-07-23 19:51:33 -0500

    update

--- src/registro.java
+++ src/registro.java
@@ -1,3 +1,62 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
 public class registro {
-    private javax.swing.JPanel JPanel;
+    public JPanel rootPanel;
+    private JTextField nombreR;
+    private JButton registrarseButton;
+    private JPasswordField contra;
+    private JTextField mensajetxt;
+    private JButton INICIARSESIÓNButton;
+    private String Nusuario;
+    private String Ncontra;
+
+    public registro() {
+        registrarseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Aquí puedes implementar la lógica para registrar al usuario
+                // Por ahora, simplemente cerraremos la ventana de registro
+                infoUsuario usuarioN = new infoUsuario();
+                Nusuario = nombreR.getText();
+                usuarioN.setUsuario(Nusuario);
+                Ncontra = String.valueOf(contra.getPassword());
+                usuarioN.setContrasenia(Ncontra);
+                try{
+                    FileOutputStream nombreF = new FileOutputStream("USUARIOS.dat",true);
+                    ObjectOutputStream obd = new ObjectOutputStream(nombreF);
+                    obd.writeObject(usuarioN);
+                    mensajetxt.setText("USUARIO REGISTRADO");
+                    nombreR.setText("");
+                    contra.setText("");
+                } catch (FileNotFoundException ex) {
+                    System.out.println("NO SE ENCUENTRA EL ARCHIVO " + ex);
+                } catch (IOException ex) {
+                    System.out.println("NO SE PUDO GUARDAR EL ARCHIVO" +ex);
+                }
+            }
+        });
+        INICIARSESIÓNButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame registroFrame = (JFrame) SwingUtilities.getWindowAncestor(rootPanel);
+                registroFrame.setVisible(false);
+
+                JFrame loginframe = new JFrame("Login");
+                inicio loginventana = new inicio();
+                loginframe.setContentPane(loginventana.JPanel);
+                loginframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                loginframe.pack();
+                loginframe.setVisible(true);
+            }
+        });
+    }
+
 }
